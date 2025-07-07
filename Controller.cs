8948319d0afb7ed56008a6b5306a70e86eb484fe
Madhu@009 <717822y127@kce.ac.in>
2025-07-07 18:27:47 +0530

    Add project files.

---
+++ Controller.cs
@@ -0,0 +1,88 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnection
+{
+
+    public class Controller
+    {
+
+        private MySqlConnection conn;
+
+        public Controller()
+        {
+        string connectionString = "Server=localhost;database=Sampledb;Uid=root;pwd='';";
+        conn = new MySqlConnection(connectionString);
+            
+        }
+       // MySqlConnection conn;
+    
+
+        public List<Student> GetStudents(int id)
+        {
+            List<Student> students = new List<Student>();
+            conn.Open();
+            string query = "Select * from students where id=@id";
+            var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            MySqlDataReader data = cmd.ExecuteReader();
+            if (data.Read())
+            {
+                students.Add(
+                    new Student
+                    {
+                        Id = Convert.ToInt32(data["id"]),
+                        Name = data["name"].ToString(),
+                        Age = Convert.ToInt32(data["age"]),
+                        department = data["department"].ToString()
+
+                    });
+            }
+            conn.Close();
+            return students;
+        }
+
+        public void AddStudent(Student student)
+        {
+            conn.Open();
+            string query = "INSERT INTO students (id,name, age, department) VALUES (@id,@name, @age, @department)";
+            var cmd = new MySqlCommand(query, conn);
+            cmd .Parameters.AddWithValue("@id", student.Id);
+            cmd.Parameters.AddWithValue("@name", student.Name);
+            cmd.Parameters.AddWithValue("@age", student.Age);
+            cmd.Parameters.AddWithValue("@department", student.department);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void DeleteStudent(int id) { 
+            conn.Open();
+            string query = "DELETE FROM students WHERE id=@id";
+            var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void UpdateStudent(Student student)
+        {
+            conn.Open();
+            string query = "UPDATE students SET name=@name, age=@age, department=@department WHERE id=@id";
+            var cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@id", student.Id);
+            cmd.Parameters.AddWithValue("@name", student.Name);
+            cmd.Parameters.AddWithValue("@age", student.Age);
+            cmd.Parameters.AddWithValue("@department", student.department);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Runtime.InteropServices;
+namespace DbConnection
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Controller s1=new Controller();
+            List<Student> result= s1.GetStudents(1);
+
+            //s1.AddStudent(new Student
+            //{
+            //    Id = 4,
+            //    Name = "John Doe",
+            //    Age = 20,
+            //    department = "Computer Science"
+            //});
+            Console.WriteLine("Enter the choice: ");
+            int choice;
+            choice = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Id of Student:");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        //result = s1.GetStudents(id);
+                        string name = Console.ReadLine();
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        string department = Console.ReadLine();
+                        s1.AddStudent(new Student
+                        {
+                            Id = id,
+                            Name = name,
+                            Age = age,
+                            department = department
+                        });
+                        break;
+                     case 2:
+                        Console.WriteLine("Enter the Id of Student to fetch:");
+                        int fetchId = Convert.ToInt32(Console.ReadLine());
+                        s1.DeleteStudent(fetchId);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter the Id of Student to fetch:");
+                        int fetchId1 = Convert.ToInt32(Console.ReadLine());
+                        result=s1.GetStudents(fetchId1);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+
+                }
+            }
+            while (choice != 0);
+           
+            foreach (var item in result)
+            {
+                Console.WriteLine($"ID: {item.Id}, Name: {item.Name},Age: {item.Age},{item.department}");
+            }
+
+
+
+
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
